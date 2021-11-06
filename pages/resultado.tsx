f1e42e8c17ee9e7e84e41a5742cf6c1c35771ba7
Rafael Borges <rafael.borges@luizalabs.com>
2021-11-05 22:43:46 -0300

    create component

---
+++ pages/resultado.tsx
@@ -0,0 +1,30 @@
+import { useRouter } from "next/router";
+import React from "react";
+import Botao from "../components/Botao";
+import Estatistica from "../components/Estatistica";
+import styles from "../styles/Resultado.module.css";
+
+export default function Resultado() {
+  const router = useRouter();
+
+  const { total, certas } = router.query;
+  const percentual = Math.round((+certas / +total) * 100);
+
+  return (
+    <div className={styles.resultado}>
+      <h1>Resultado final</h1>
+
+      <div style={{ display: "flex" }}>
+        <Estatistica texto="Perguntas" valor={total} />
+        <Estatistica texto="Certas" valor={certas} corFundo="#9CD2A4" />
+        <Estatistica
+          texto="Percentual"
+          valor={`${percentual}%`}
+          corFundo="#DE6A33"
+        />
+      </div>
+
+      <Botao href="/" texto="Reiniciar" />
+    </div>
+  );
+}
